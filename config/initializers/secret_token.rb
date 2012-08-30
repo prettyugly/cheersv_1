e0e539eb6893a3a18b57d524377156d05d7b9b80
Al Rivera <prettyuglyproductions@gmail.com>
2012-08-30 11:11:16 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cheersv1::Application.config.secret_token = '6cf56959f7cd1a531586690849b4a13f1fea1a5fd997ada60e4e7a2683ca3566e397d752f6f0c92eee6b757a6fc21fac2f608810b5b47c8f9964dfc1e27b74ce'
